001de97ee659cd7de21ead61da5c5bfcb04e559d
Serial Lab <slab@serial-lab.com>
2018-02-23 13:50:46 -0700

    Deleted cruft.  Fixed some celery issues.

--- qu4rtet/taskapp/celery.py
+++ qu4rtet/taskapp/celery.py
@@ -19,46 +19,36 @@ class CeleryConfig(AppConfig):
     def ready(self):
         app.config_from_object('django.conf:settings', namespace='CELERY')
         app.autodiscover_tasks(force=True)
-        print('*')
         # Using a string here means the worker will not have to
         # pickle the object when using Windows.
-        # if hasattr(settings, 'RAVEN_CONFIG'):
+        if hasattr(settings, 'RAVEN_CONFIG'):
             # Celery signal registration
-# Since raven is required in production only,
+            # Since raven is required in production only,
             # imports might (most surely will) be wiped out
             # during PyCharm code clean up started
             # in other environments.
-            # @formatter:off
-            # from raven import Client as RavenClient
-            # from raven.contrib.celery import register_signal as raven_register_signal
-            # from raven.contrib.celery import register_logger_signal as raven_register_logger_signal
-# @formatter:on
+            from raven import Client as RavenClient
+            from raven.contrib.celery import register_signal as raven_register_signal
+            from raven.contrib.celery import register_logger_signal as raven_register_logger_signal
 
-            # raven_client = RavenClient(dsn=settings.RAVEN_CONFIG['DSN'])
-            # raven_register_logger_signal(raven_client)
-            # raven_register_signal(raven_client)
+            raven_client = RavenClient(dsn=settings.RAVEN_CONFIG['DSN'])
+            raven_register_logger_signal(raven_client)
+            raven_register_signal(raven_client)
 
-        # if hasattr(settings, 'OPBEAT'):
-# Since opbeat is required in production only,
+        if hasattr(settings, 'OPBEAT'):
+            # Since opbeat is required in production only,
             # imports might (most surely will) be wiped out
             # during PyCharm code clean up started
             # in other environments.
-            # @formatter:off
-            # from opbeat.contrib.django.models import client as opbeat_client
-            # from opbeat.contrib.django.models import logger as opbeat_logger
-            # from opbeat.contrib.django.models import register_handlers as opbeat_register_handlers
-            # from opbeat.contrib.celery import register_signal as opbeat_register_signal
-# @formatter:on
-#
-#             try:
-#                 opbeat_register_signal(opbeat_client)
-#             except Exception as e:
-#                 opbeat_logger.exception('Failed installing celery hook: %s' % e)
-#
-#             if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
-#                 opbeat_register_handlers()
+            from opbeat.contrib.django.models import client as opbeat_client
+            from opbeat.contrib.django.models import logger as opbeat_logger
+            from opbeat.contrib.django.models import register_handlers as opbeat_register_handlers
+            from opbeat.contrib.celery import register_signal as opbeat_register_signal
 
+            try:
+                opbeat_register_signal(opbeat_client)
+            except Exception as e:
+                opbeat_logger.exception('Failed installing celery hook: %s' % e)
 
-# @app.task(bind=True)
-# def debug_task(self):
-#     print('Request: {0!r}'.format(self.request))  # pragma: no cover
+            if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
+                opbeat_register_handlers()
