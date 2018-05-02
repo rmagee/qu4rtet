c1c3edde7fb4b61e8a6b953fa1d7afd6920b04d2
Loic Duros <loic.duros@gmail.com>
2018-05-02 10:51:47 -0400

    indentation

--- config/settings/base.py
+++ config/settings/base.py
@@ -306,5 +306,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.SearchFilter', 'rest_framework.filters.OrderingFilter', 'django_filters.rest_framework.DjangoFilterBackend')
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ),
 }
