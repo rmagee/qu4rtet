f72f142c29901bf328b55aa52744563c23bdeca4
Serial Lab <slab@serial-lab.com>
2018-05-05 15:17:05 -0600

    updated default settings module.

--- qu4rtet/taskapp/celery.py
+++ qu4rtet/taskapp/celery.py
@@ -7,8 +7,8 @@ from django.conf import settings
 
 if not settings.configured:
     # set the default Django settings module for the 'celery' program.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')  # pragma: no cover
-    print('Using local settings.')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')  # pragma: no cover
+    print('Using production settings.')
 
 app = Celery('qu4rtet')
 
