954b9c3e775c5de18f983a793eeec63bc6fad62c
Serial Lab <slab@serial-lab.com>
2018-05-24 11:34:35 -0600

    Updated utility script.

--- utility/update-quartet.sh
+++ utility/update-quartet.sh
@@ -3,6 +3,7 @@
 cd /srv/qu4rtet
 git checkout master
 git pull https://gitlab.com/serial-lab/qu4rtet.git master
+git fetch --tags
 export LATESTQ4=`git describe --abbrev=0 --tags`
 git checkout tags/$LATESTQ4
 sudo pip3 install -r ./requirements/production.txt
@@ -10,4 +11,12 @@ sudo python3 manage.py makemigrations
 sudo python3 manage.py migrate
 sudo python3 manage.py migrate --run-syncdb
 sudo python3 manage.py collectstatic
+sudo pip3 install eparsecis --upgrade
+sudo pip3 install epcpyyes --upgrade
+sudo pip3 install quartet_capture --upgrade
+sudo pip3 install quartet_epcis --upgrade
+sudo pip3 install serialbox --upgrade
+sudo pip3 install random_flavorpack --upgrade
+sudo pip3 install quartet_manifest --upgrade
+sudo pip3 install quartet_masterdata --upgrade
 sudo restart-quartet
