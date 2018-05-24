cd19901f0ed7974504ce5728bcbeeb46b3671321
Serial Lab <slab@serial-lab.com>
2018-05-24 11:09:57 -0600

    updated utility script

--- utility/update-quartet.sh
+++ utility/update-quartet.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 cd /srv/qu4rtet
-git pull
+git checkout master
+git pull https://gitlab.com/serial-lab/qu4rtet.git master
 export LATESTQ4=`git describe --abbrev=0 --tags`
 git checkout tags/$LATESTQ4
 sudo pip3 install -r ./requirements/production.txt
