4d34b3215d87abbf3e57a1d1f547811a8105e852
Serial Lab <slab@serial-lab.com>
2018-07-12 10:28:33 -0600

    Updated requirements pinning and new requirement file for pypy unit testing.

--- requirements/update.sh
+++ requirements/update.sh
@@ -3,5 +3,6 @@ echo "Updating QU4RTET dependency files..."
 pip-compile test.in -o test.txt --upgrade
 pip-compile local.in -o local.txt --upgrade
 pip-compile production.in -o production.txt --upgrade
+pip-compile pypy.in -o pypy.txt --upgrade
 echo "Complete."
 
