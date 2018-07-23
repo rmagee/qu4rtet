2e6b7324d4ce794f36700032685ff26f7e801a19
Serial Lab <slab@serial-lab.com>
2018-07-23 11:35:41 -0600

    Update to add execute for group+owner.

--- utility/add-utils-to-bin.sh
+++ utility/add-utils-to-bin.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Adding QU4RTET commands to the system path"
+chmod ag+x -R /srv/qu4rtet/utility/*.sh
 ln -s /srv/qu4rtet/utility/restart-quartet.sh /usr/local/bin/restart-quartet
 ln -s /srv/qu4rtet/utility/restart-quartet.sh /usr/local/bin/stop-quartet
 ln -s /srv/qu4rtet/utility/restart-quartet.sh /usr/local/bin/update-quartet
