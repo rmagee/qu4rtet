de277ce0128a7ebcfec204f22a3c983ece92e95b
Serial Lab <slab@serial-lab.com>
2018-07-31 15:08:34 -0600

    updated commandˆ

--- utility/restart-quartet.sh
+++ utility/restart-quartet.sh
@@ -1,6 +1,8 @@
 # assumes you set up using the ubuntu/debian setup instructions
 sudo supervisorctl stop guni:gunicorn
 sudo supervisorctl start guni:gunicorn
+sudo supervisorctl stop flower
+sudo supervisorctl start flower
 sudo systemctl restart nginx
 sudo /etc/init.d/celeryd stop
 sudo /etc/init.d/celeryd start
