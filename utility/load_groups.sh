60bdb0772d6c857fad1b7b091a6cc6683d8743e7
Ubuntu <ubuntu@ip-172-31-43-142.ec2.internal>
2018-08-01 20:29:58 +0000

    utility script to create groups

--- utility/load_groups.sh
+++ utility/load_groups.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-../
+python manage.py create_capture_groups
+python manage.py create_epcis_groups
+python manage.py create_masterdata_groups
+python manage.py create_output_groups
+python manage.py load_serialbox_auth
+python manage.py load_random_flavorpack_auth
+
