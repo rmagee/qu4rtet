ca0fcd1eaf216a9df5b03a2719511892287fbe49
Serial Lab <slab@serial-lab.com>
2018-08-08 08:23:01 -0600

    updated script

--- utility/ec2_create_database.sh
+++ utility/ec2_create_database.sh
@@ -6,4 +6,7 @@ POSTGRES_PORT=${POSTGRES_PORT:=5432}
 echo "Postgres Port=$POSTGRES_PORT"
 createdb -e -E UTF8 -O $POSTGRES_USER -U $POSTGRES_USER --host=$DATABASE_HOST --port=$POSTGRES_PORT $POSTGRES_DB 'The QU4RTET database backend.'
 echo "Running migrations..."
-python manage.py migrate
+python manage.py migrate --no-input
+python manage.py migrate --no-input --run-syncdb
+echo "Creatging superuser..."
+echo "from django.contrib.auth.models import User; User.objects.create_superuser(\"$DJANGO_SUPERUSER\", \"$DJANGO_SUPERUSER_EMAIL\", \"$DJANGO_SUPERUSER_PASSWORD\")" | python manage.py shell
