e9879e6f30dd9783481ed022d90d0cfa1c49d422
Serial Lab <slab@serial-lab.com>
2018-08-08 08:12:02 -0600

    utility script

--- utility/ec2_create_database.sh
+++ utility/ec2_create_database.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+echo "Importing environment variables"
+set -o allexport; source /tmp/q4_env; set +o allexport
+echo "Creating the quartet database..."
+POSTGRES_PORT=${POSTGRES_PORT:=5432}
+echo "Postgres Port=$POSTGRES_PORT"
 createdb -e -E UTF8 -O $POSTGRES_USER -U $POSTGRES_USER --host=$DATABASE_HOST --port=$POSTGRES_PORT $POSTGRES_DB 'The QU4RTET database backend.'
+echo "Running migrations..."
+python manage.py migrate
