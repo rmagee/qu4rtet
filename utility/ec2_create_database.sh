313e53b1c2ec30c21a24f73037df877daba9d6af
Serial Lab <slab@serial-lab.com>
2018-08-09 11:53:29 -0600

    Updated utility script.

--- utility/ec2_create_database.sh
+++ utility/ec2_create_database.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-echo "Importing environment variables"
-set -o allexport; source /tmp/q4_env; set +o allexport
-echo "Creating the quartet database..."
-sudo su postgres
-POSTGRES_PORT=${POSTGRES_PORT:=5432}
-echo "Postgres Port=$POSTGRES_PORT"
-createdb -e -E UTF8 -O $POSTGRES_USER -U $POSTGRES_USER --host=$DATABASE_HOST --port=$POSTGRES_PORT $POSTGRES_DB 'The QU4RTET database backend.'
-exit
-echo "Running migrations..."
+/home/ubuntu/.virtualenvs/qu4rtet/bin/python manage.py create_database
+echo "Performing migrations..."
 /home/ubuntu/.virtualenvs/qu4rtet/bin/python manage.py migrate --no-input
 /home/ubuntu/.virtualenvs/qu4rtet/bin/python manage.py migrate --no-input --run-syncdb
-echo "Creatging superuser..."
-echo "from django.contrib.auth.models import User; User.objects.create_superuser(\"$DJANGO_SUPERUSER\", \"$DJANGO_SUPERUSER_EMAIL\", \"$DJANGO_SUPERUSER_PASSWORD\")" | /home/ubuntu/.virtualenvs/qu4rtet/bin/python manage.py shell
+echo "Creating superuser..."
+/home/ubuntu/.virtualenvs/qu4rtet/bin/python manage.py create_super_from_env --no-input --run-syncdb
+echo "Complete."
