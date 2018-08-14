d2ce92a481d42d5e5ec692512506acab5dd7d53f
Serial Lab <slab@serial-lab.com>
2018-08-14 15:39:39 -0600

    added https utility for certbot

---
+++ utility/enable-https.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update -y
+sudo apt-get install python-certbot-nginx -y
+sudo certbot --nginx
