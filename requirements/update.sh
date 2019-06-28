a39bf86982ad556e78347197a33738d06862d8a4
Serial Lab <slab@serial-lab.com>
2019-06-28 13:14:21 -0600

    update requirements files

--- requirements/update.sh
+++ requirements/update.sh
@@ -7,5 +7,6 @@ pip-compile production.in -o ../requirements.txt --upgrade
 pip-compile pypy.in -o pypy.txt --upgrade
 pip-compile ec2.in -o ec2.txt --upgrade
 pip-compile serial-lab.in -o serial-lab.txt --upgrade
+pip-compile pypy-test.in -o pypy-test.txt --upgrade
 echo "Complete."
 
