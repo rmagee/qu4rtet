1b6210f24a060b7363e7de3d6867fc3dd5d2a859
Serial Lab <slab@serial-lab.com>
2019-11-15 10:45:08 -0700

    added setup utility for developers

---
+++ utility/setup-dev-environment.sh
@@ -0,0 +1,26 @@
+git clone https://gitlab.com/serial-lab/qu4rtet.git
+
+git clone https://gitlab.com/serial-lab/quartet_epcis.git
+git clone https://gitlab.com/serial-lab/quartet_capture.git
+git clone https://gitlab.com/serial-lab/quartet_output.git
+git clone https://gitlab.com/serial-lab/quartet_masterdata.git
+git clone https://gitlab.com/serial-lab/quartet_integrations.git
+git clone https://gitlab.com/serial-lab/quartet_templates.git
+git clone https://gitlab.com/serial-lab/quartet_vrs.git
+git clone https://gitlab.com/serial-lab/quartet_trail.git
+git clone https://gitlab.com/serial-lab/EPCPyYes.git
+git clone https://gitlab.com/serial-lab/EParseCIS.git
+git clone https://gitlab.com/serial-lab/gs123.git
+
+pip install -e ./quartet_epcis
+pip install -e ./quartet_capture
+pip install -e ./quartet_output
+pip install -e ./quartet_masterdata
+pip install -e ./quartet_integrations
+pip install -e ./quartet_templates
+pip install -e ./quartet_vrs
+pip install -e ./quartet_trail
+pip install -e ./EPCPyYes
+pip install -e ./EParseCIS
+pip install -e ./gs123
+
