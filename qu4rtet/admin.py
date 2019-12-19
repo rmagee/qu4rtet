23ad47e6b788cffa1478223f12fec2426c136b79
Serial Lab <slab@serial-lab.com>
2019-12-19 09:59:20 -0700

    Removed reference

--- qu4rtet/admin.py
+++ qu4rtet/admin.py
@@ -22,7 +22,6 @@ from quartet_templates import admin as t_admin
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib import admin
-from warewolf_api import admin as ww_admin
 
 class QuartetAdminSite(admin.AdminSite):
     site_header = "QU4RTET Administration"
