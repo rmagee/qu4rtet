d3c74c89e2c9a245e099bedf31a62e32cd3a036d
Serial Lab <slab@serial-lab.com>
2019-12-25 12:56:25 -0700

    simplified status output.

--- utility/restart-quartet.sh
+++ utility/restart-quartet.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # assumes you set up using the ubuntu/debian setup instructions
+#!/usr/bin/env bash
+# assumes you set up using the ubuntu/debian setup instructions
 sudo systemctl restart gunicorn.socket
-sudo systemctl status gunicorn.socket
 sudo systemctl restart gunicorn.service
-sudo systemctl status gunicorn.service
 sudo systemctl restart flower.service
-sudo systemctl status flower.service
 sudo systemctl restart nginx
-sudo systemctl status nginx
 sudo systemctl restart celery.service
-sudo systemctl status celery.service
+sudo systemctl status celery nginx flower gunicorn
+
